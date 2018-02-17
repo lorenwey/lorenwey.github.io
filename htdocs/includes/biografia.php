d7b1ceb94a5738eda06d535840006483f1783776
lorenwey <munslorena@gmail.com>
2018-02-17 18:58:35 +0100

    todo

---
+++ htdocs/includes/biografia.php
@@ -0,0 +1,17 @@
+
+<!-- Biografía -->
+<section id="biografia">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Biografía</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+
+            </div>
+        </div>
+    </div>
+</section>
