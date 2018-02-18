f040e23255557dc014e4c30eb2f79aefef67b1e5
lorenwey <munslorena@gmail.com>
2018-02-18 12:14:45 +0100

    sd

--- htdocs/index.php
+++ htdocs/index.php
@@ -441,11 +441,13 @@
 										<li data-target="#myCarousel6" data-slide-to="2"></li>
 										<li data-target="#myCarousel6" data-slide-to="3"></li>
 										<li data-target="#myCarousel6" data-slide-to="4"></li>
+										<li data-target="#myCarousel6" data-slide-to="5"></li>
 									</ol>
 
 									<!-- Wrapper for slides -->
 									<div class="carousel-inner">
 										<div class="item active"><img src="img/portfolio/tienda.png" alt="tienda"/></div>
+										<div class="item"><img src="img/portfolio/tienda1.png" alt="tienda1"/></div>
 										<div class="item"><img src="img/portfolio/tienda2.png" alt="tienda2"/></div>
 										<div class="item"><img src="img/portfolio/tienda3.png" alt="tienda3"/></div>
 										<div class="item"><img src="img/portfolio/tienda4.png" alt="tienda4"/></div>
